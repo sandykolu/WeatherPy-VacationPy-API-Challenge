af953439aea9485544b3e24d293c829576c8ded2
sandykolu <sandeep.kolwalkar@gmail.com>
2021-01-30 01:08:09 -0600

    final plots

--- api_keys.py
+++ api_keys.py
@@ -1,9 +1,6 @@
 # OpenWeatherMap API Key
 
-weather_api_key = "4484cc7db9aafd8fbb7202ddc9908223"
-# weather_api_key = "5513911c6b3c270d32dbc2f560f613fb"
-
-
+weather_api_key = ""
 
 # Google API Key
 g_key = "YOUR KEY HERE!"
